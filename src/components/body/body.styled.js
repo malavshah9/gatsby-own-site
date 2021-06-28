d1b80375f1af6710206a6c202c6eb6a21e337417
Malav Shah <mdshah9574@gmail.com>
2021-06-28 18:53:08 +0530

    fix: responsiveness for mobile view

--- src/components/body/body.styled.js
+++ src/components/body/body.styled.js
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components"
 
+const mobileView = "610px"
 const StyledFlexHalf = css`
   display: flex;
   justify-content: center;
@@ -14,15 +15,28 @@ export const StyledTextWrapper = styled.div`
 
 export const StyledWrapperLeft = styled.div`
   ${StyledFlexHalf}
+  @media (max-width: ${mobileView}) {
+    margin-top: 100px;
+    height: 55%;
+  }
 `
 export const StyledMainWrapper = styled.div`
   display: flex;
   flex-direction: row;
   height: 100%;
+  @media (max-width: ${mobileView}) {
+    flex-direction: column;
+  }
 `
 
 export const StyledWrapperRight = styled.div`
   ${StyledFlexHalf}
   align-items:center;
   font-size: ${({ theme }) => theme.fontSizeMidHeading};
+  @media (max-width: ${mobileView}) {
+    margin-top: 80px;
+    margin-right: 100%;
+    margin-bottom: 80px;
+    width: 100%;
+  }
 `
